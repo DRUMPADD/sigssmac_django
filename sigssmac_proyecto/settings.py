65bd1f13312293a04c72d300522fcb777aeaac79
Erick <erickfco1999m@gmail.com>
2022-01-27 01:58:50 -0600

    configuraciones

--- sigssmac_proyecto/settings.py
+++ sigssmac_proyecto/settings.py
@@ -39,11 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "sigss_principal",
+    "sigss_principal.apps.SigssPrincipalConfig",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
