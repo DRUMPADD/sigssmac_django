69ca3c83c6e7d49ae6fc6cdba2060d5a143a1f5b
Erick <erickfco1999m@gmail.com>
2022-01-31 16:17:49 -0600

    funcion enviar mensaje

--- sigss_principal/views.py
+++ sigss_principal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from django.db import connection
 
 # Create your views here.
 def principal(request):
@@ -7,8 +8,15 @@ def principal(request):
 
 def enviar_mensaje(request):
     if request.method == 'POST':
-        nombre = request.POST.get("nombre") + request.POST.get("apellidos")
+        cursor = connection.cursor()
+        nombre = request.POST.get("nombre")
+        apellidos = str(request.POST.get("apellidos")).split(" ")
         email_ = request.POST.get("email")
-        mensaje = "Nombre: " + str(nombre) + "\n" + "Mensaje: " + request.POST.get("mensaje") + "\nContacto: " + email_
-        send_mail(subject="Solicitud de cotización", message=mensaje, from_email=email_, recipient_list=["ventas.sigssmac@gmail.com", "sti1.sigssmac@outlook.com", "ventas.sigssmac@outlook.com"], fail_silently=False)
+        mensaje = request.POST.get("mensaje")
+        
+        cursor.callproc("CONTACTO", [email_, nombre, apellidos[0], apellidos[1], mensaje])
+        
+        mensaje_email = "Nombre: " + str(nombre) +" " + apellidos[0] + " " + apellidos[1] + "\n" + "Mensaje: " + mensaje + "\nContacto: " + email_
+        
+        send_mail(subject="Solicitud de cotización", message=mensaje_email, from_email=email_, recipient_list=["ventas.sigssmac@gmail.com", "sti1.sigssmac@outlook.com", "ventas.sigssmac@outlook.com"], fail_silently=False)
         return redirect("inicio")
