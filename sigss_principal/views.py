018742d21ba34aca40ad252ef8e5c6009bd5ad76
Erick <erickfco1999m@gmail.com>
2022-02-08 15:29:54 -0600

    validando apellidos

--- sigss_principal/views.py
+++ sigss_principal/views.py
@@ -13,10 +13,12 @@ def enviar_mensaje(request):
         apellidos = str(request.POST.get("apellidos")).split(" ")
         email_ = request.POST.get("email")
         mensaje = request.POST.get("mensaje")
-        
-        cursor.callproc("CONTACTO", [email_, nombre, apellidos[0], apellidos[1], mensaje])
-        
-        mensaje_email = "Nombre: " + str(nombre) +" " + apellidos[0] + " " + apellidos[1] + "\n" + "Mensaje: " + mensaje + "\nContacto: " + email_
-        
+        if len(apellidos) == 1:
+            cursor.callproc("CONTACTO", [email_, nombre, apellidos[0], "", mensaje])
+            mensaje_email = "Nombre: " + str(nombre) +" " + apellidos[0] + "\n" + "Mensaje: " + mensaje + "\nContacto: " + email_
+        else:
+            cursor.callproc("CONTACTO", [email_, nombre, apellidos[0], apellidos[1], mensaje])
+            mensaje_email = "Nombre: " + str(nombre) +" " + apellidos[0] + " " + apellidos[1] + "\n" + "Mensaje: " + mensaje + "\nContacto: " + email_
+
         send_mail(subject="Solicitud de cotización", message=mensaje_email, from_email=email_, recipient_list=["ventas.sigssmac@gmail.com", "sti1.sigssmac@outlook.com", "ventas.sigssmac@outlook.com"], fail_silently=False)
         return redirect("inicio")
