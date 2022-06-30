3127a86d101bd379444e890d2df2271c7fa4ad85
Erick <erickfco1999m@gmail.com>
2022-06-30 13:33:02 -0500

    mostrar mensajes sweetalert y recarga de datos en tabla

--- static/js/tarco.js
+++ static/js/tarco.js
@@ -7,8 +7,6 @@ var values = ["Malo", "Regular", "Bueno"];
 
 
 var table = document.getElementById("tb_items"), rIndex;
-
-// Botones
 let btnEnviar = document.getElementById("btnEnviar");
 let btnLimpiar = document.getElementById("btnLimpiar");
 let btnEliminar = document.getElementById("btnEliminar");
@@ -30,10 +28,15 @@ let formulario = new FormData(document.getElementById("form_item"));
 let form_r = document.getElementById("form_item");
 
 
-function reload(key){
-    var container = document.getElementById(key);
-    var content = container.innerHTML;
-    container.innerHTML= content; 
+function reload(){
+    let filas_tabla = table.getElementsByTagName("tr");
+    var filas = filas_tabla.length;
+    for(let x = filas - 1; x > 0; x--) {
+        if(table.querySelector("tbody").hasChildNodes()) {
+            table.querySelector("tbody").removeChild(filas_tabla[x])
+        }
+    }
+    getItems2();
 }
 
 for(const val of values) {
@@ -60,59 +63,55 @@ function getCookie(name) {
 }
 
 async function getItems2() {
-    try {
-        const response = await fetch("/getInfo")
-        const data = await response.json();
-        
-        
-        const items = new Array(data.info);
+    const response = await fetch("/getInfo")
+    const data = await response.json();
+    
+    
+    const items = new Array(data.info);
 
-        items[0].forEach(element => {
-            const tr = document.createElement("tr");
+    items[0].forEach(element => {
+        const tr = document.createElement("tr");
 
-            const f_ac = element[5] != null ? element[5]: '';
-            const fech_ac_formato = f_ac.split('-');
-            const fecha_act = new Date(fech_ac_formato[0], fech_ac_formato[1], fech_ac_formato[2]);
-            const cal_fech_ac = Math.round((fecha_act.getTime() - Date.now())/ (1000*60*60*24));
-            tr.innerHTML += `
-                <td>${element[0]}</td>
-                <td>${element[1]}</td>
-                <td>${element[2]}</td>
-                <td>${element[3]}</td>
-                <td>${element[4]}</td>
-                <td class='bg-${cal_fech_ac > element[4] ? 'primary': 'dark '} text-white'>${element[5] == null ? '': element[5]}</td>
-                <td>${element[6]}</td>
-                <td>${element[7] == null ? '': element[7]}</td>
-                <td>${element[8] == null ? '': element[8]}</td>
-                <td class='bg-${element[9] == 'Malo' ? "primary": element[9] == 'Regular' ? "warning": "success"}'>${element[9]}</td>
-            `;
+        const f_ac = element[5] != null ? element[5]: '';
+        const fech_ac_formato = f_ac.split('-');
+        const fecha_act = new Date(fech_ac_formato[0], fech_ac_formato[1], fech_ac_formato[2]);
+        const cal_fech_ac = Math.round((fecha_act.getTime() - Date.now())/ (1000*60*60*24));
+        tr.innerHTML += `
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td>${element[2]}</td>
+            <td>${element[3]}</td>
+            <td>${element[4]}</td>
+            <td class='bg-${cal_fech_ac > element[4] ? 'danger': 'dark '} text-white'>${element[5] == null ? '': element[5]}</td>
+            <td>${element[6]}</td>
+            <td>${element[7] == null ? '': element[7]}</td>
+            <td>${element[8] == null ? '': element[8]}</td>
+            <td class='bg-${element[9] == 'Malo' ? "danger": element[9] == 'Regular' ? "warning": "success"}'>${element[9]}</td>
+        `;
 
-            tb_info.appendChild(tr);
-        });
-        
-        for(let i = 1; i < table.rows.length; i++) {
-            if(table.rows[i].cells != 10) {
-                table.rows[i].onclick = function(e) {
-                    inp_id.value = this.cells[0].innerHTML;
-                    inp_id.setAttribute("disabled", "");
-                    inp_nombre.value = this.cells[1].innerHTML;
-                    inp_car.value = this.cells[2].innerHTML;
-                    inp_ubi.value = this.cells[3].innerHTML;
-                    inp_frec.value = this.cells[4].innerHTML;
-                    inp_fec_ins.value = this.cells[5].innerHTML;
-                    inp_conds.value = this.cells[6].innerHTML;
-                    inp_fec_col.value = this.cells[7].innerHTML;
-                    inp_fec_reem.value = this.cells[8].innerHTML;
-                    inp_cond.value = this.cells[9].innerHTML;
-
-                    opcion_post.value = 'ACTUALIZAR';
-                    btnLimpiar.removeAttribute("disabled");
-                    btnEliminar.removeAttribute("disabled");
-                }
+        tb_info.appendChild(tr);
+    });
+    
+    for(let i = 1; i < table.rows.length; i++) {
+        if(table.rows[i].cells != 10) {
+            table.rows[i].onclick = function(e) {
+                inp_id.value = this.cells[0].innerHTML;
+                inp_id.setAttribute("disabled", "");
+                inp_nombre.value = this.cells[1].innerHTML;
+                inp_car.value = this.cells[2].innerHTML;
+                inp_ubi.value = this.cells[3].innerHTML;
+                inp_frec.value = this.cells[4].innerHTML;
+                inp_fec_ins.value = this.cells[5].innerHTML;
+                inp_conds.value = this.cells[6].innerHTML;
+                inp_fec_col.value = this.cells[7].innerHTML;
+                inp_fec_reem.value = this.cells[8].innerHTML;
+                inp_cond.value = this.cells[9].innerHTML;
+                
+                opcion_post.value = 'ACTUALIZAR';
+                btnLimpiar.removeAttribute("disabled");
+                btnEliminar.removeAttribute("disabled");
             }
         }
-    } catch(e) {
-        return
     }
 }
 
@@ -149,17 +148,14 @@ function enviar() {
         return res.json();
     })
     .then(data => {
-        alert(data.msg);
+        return data.msg;
     })
     .catch(err => {
         return
     })
-
-    console.log(formulario);
 }
 
 function modificar() {
-    let mensaje = "";
     fetch("/actualizarItem", {
         method: "POST",
         headers: {
@@ -184,17 +180,14 @@ function modificar() {
         return res.json();
     })
     .then(data => {
-        mensaje = data.msg;
+        return data.msg;
     })
     .catch(err => {
         return
     })
-
-    return mensaje;
 }
 
 function eliminar() {
-    let mensaje = "";
     fetch("/eliminarItem", {
         method: "POST",
         headers: {
@@ -219,64 +212,71 @@ function eliminar() {
         return res.json();
     })
     .then(data => {
-        mensaje = data.msg;
+        return data.msg;
     })
     .catch(err => {
         return
     })
-    return mensaje;
 }
-
-
-function limpiarForm() {
-    inp_id.value = "";
-    inp_nombre.value = "";
-    inp_car.value = "";
-    inp_ubi.value = "";
-    inp_frec.value = "";
-    inp_fec_ins.value = "";
-    inp_conds.value = "";
-    inp_fec_col.value = "";
-    inp_fec_reem.value = "";
-    inp_cond.value = "";
-}
-
 
 btnEnviar.addEventListener("click", (e) => {
     e.preventDefault();
-    let res = "";
     if(validar()) {
         if (opcion_post.value == 'AGREGAR') {
-            res = enviar();
+            enviar();
+            Swal.fire(
+                'Agregado!',
+                'El item ha sido agregado.',
+                'success'
+            )
             opcion_post.value = 'AGREGAR';
         } else {
-            res = modificar();
+            modificar();
+            Swal.fire(
+                'Agregado!',
+                'El item ha sido modificado.',
+                'success'
+            )
         }
         form_r.reset();
-        reload("tb_items");
-        alert(res);
+        reload();
     } else {
         alert("Debe llenar el formulario")
     }
+
 })
 
 btnLimpiar.addEventListener("click", () => {
-    limpiarForm();
+    form_r.reset();
     btnLimpiar.setAttribute("disabled", "");
     btnEliminar.setAttribute("disabled", "");
     inp_id.removeAttribute("disabled");
     opcion_post.value = 'AGREGAR';
 })
 
-btnEliminar.addEventListener("click", async (e) => {
+btnEliminar.addEventListener("click", (e) => {
     e.preventDefault();
-    let res = "";
     if(validar()) {
-        if(confirm("¿Está seguro de que desea eliminar este item?\nLos datos no se podrán recuperar")) {
-            res = eliminar();
-            form_r.reset();
-            await reload("tb_items");
-            alert(res);
-        }
+        Swal.fire({
+            title: 'Está seguro de eliminar el item?',
+            text: "La acción no se podrá revertir!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: 'Cancelar',
+            }).then(async (result) => {
+            if (result.isConfirmed) {
+                eliminar();
+                reload();
+                form_r.reset();
+                Swal.fire(
+                    'Eliminado!',
+                    'El item ha sido eliminado.',
+                    'success'
+                )
+            }
+        })
     }
 })
