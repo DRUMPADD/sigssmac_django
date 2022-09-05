e1b4bf45c2b4c8036e5b7a93de4a1e9c305a2126
Erick <erickfco1999@gmail.com>
2022-09-05 15:10:42 -0500

    Agregando urls de vistas para plantilla

--- sigss_principal/urls.py
+++ sigss_principal/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from sigss_principal.views import *
-
+from .views import *
+from sigss_principal.plataforma.views import *
 
 urlpatterns = [
     path("", principal, name="inicio"),
@@ -14,4 +14,9 @@ urlpatterns = [
     path("agregarItem", enviar_item, name="enviar_item"),
     path("actualizarItem", modificar_item, name="actualizar_item"),
     path("eliminarItem", eliminar_item, name="eliminar_item"),
+    path("plataforma/general", general_view, name="principal"),
+    path("plataforma/activitidades", activities_view, name="actividades"),
+    path("plataforma/equipo", items_view, name="items"),
+    # path("mantenimiento_preventivo", manteinment_1_view, name="manteinment1"),
+    path("plataforma/mantenimiento_correctivo", manteinment_view, name="manteinment"),
 ]
