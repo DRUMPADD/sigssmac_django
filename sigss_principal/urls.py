770520945183ce0047d3fc6daac56ebff9c273e2
Erick <erickfco1999@gmail.com>
2022-09-13 09:44:25 -0500

    Incorporando urls de peticiones de actividades

--- sigss_principal/urls.py
+++ sigss_principal/urls.py
@@ -16,10 +16,15 @@ urlpatterns = [
     path("agregarItem", enviar_item, name="enviar_item"),
     path("actualizarItem", modificar_item, name="actualizar_item"),
     path("eliminarItem", eliminar_item, name="eliminar_item"),
+    # ? General manteinment requests
     path("plataforma/general", general_view, name="principal"),
+    # ? Activities requests
     path("plataforma/actividades", activities_view, name="actividades"),
     path("plataforma/actividades/crear_actividad", act_req.create_activity, name="crear_actividad"),
+    path("activities/getActivities", act_req.create_activity, name="crear_actividad"),
+    # ? Items requests
     path("plataforma/equipo", items_view, name="items"),
     # path("mantenimiento_preventivo", manteinment_1_view, name="manteinment1"),
+    # ? Manteinment requests
     path("plataforma/mantenimiento_correctivo", manteinment_view, name="manteinment"),
 ]
