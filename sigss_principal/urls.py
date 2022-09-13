9ef2a870e8d279a101fad6d25b3c047eaf6565f6
Erick <erickfco1999@gmail.com>
2022-09-13 09:09:37 -0500

    Empezando a agregar urls

--- sigss_principal/urls.py
+++ sigss_principal/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import *
 from sigss_principal.plataforma.views import *
+# ? Activities requests
+import sigss_principal.requests.activity_requests as act_req
 
 urlpatterns = [
     path("", principal, name="inicio"),
@@ -15,7 +17,8 @@ urlpatterns = [
     path("actualizarItem", modificar_item, name="actualizar_item"),
     path("eliminarItem", eliminar_item, name="eliminar_item"),
     path("plataforma/general", general_view, name="principal"),
-    path("plataforma/activitidades", activities_view, name="actividades"),
+    path("plataforma/actividades", activities_view, name="actividades"),
+    path("plataforma/actividades/crear_actividad", act_req.create_activity, name="crear_actividad"),
     path("plataforma/equipo", items_view, name="items"),
     # path("mantenimiento_preventivo", manteinment_1_view, name="manteinment1"),
     path("plataforma/mantenimiento_correctivo", manteinment_view, name="manteinment"),
