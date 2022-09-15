e8a03dc8a31b3e8dfb8a520471934a4328886dae
Erick <erickfco1999@gmail.com>
2022-09-15 16:53:24 -0500

    Corrigiendo error de funcion no contenida en archivo de items_requests

--- sigss_principal/requests/items_requests.py
+++ sigss_principal/requests/items_requests.py
@@ -3,6 +3,16 @@ from django.db import connection
 from django.db import InternalError as errors, IntegrityError as errors, InterfaceError as errors, ProgrammingError as errors
 import json
 
+def show_items(request):
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM maquinas_equipos")
+        items = cursor.fetchall()
+        return JsonResponse({"msg": items}, status=200)
+    except (errors) as e:
+        print(e)
+        return JsonResponse({"msg": None}, status=200)
+
 def create_item(request):
     if request.method == 'POST':
         responses = json.loads(request.body.decode("utf-8"))
