064af8d35d7579e3f87956c570da0a5826885fb8
Erick <erickfco1999@gmail.com>
2022-09-15 18:14:11 -0500

    Agregando funcion getCookies

--- static/js/items.js
+++ static/js/items.js
@@ -4,6 +4,22 @@ let btnClose = document.querySelector(".btnClose");
 let t_body = document.getElementById("items");
 let form = document.querySelector("#form-item");
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 function hideUpdateForm() {
     box_update_form.style.visibility = "hidden";
     box_update_form.style.opacity = 0;
