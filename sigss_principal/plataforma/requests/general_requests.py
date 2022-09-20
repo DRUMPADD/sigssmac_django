0dfd876378f93598b3bc2ccd0b607c514d316577
Erick <erickfco1999@gmail.com>
2022-09-20 02:19:45 -0500

    Quitando validacion de metodo post por get

--- sigss_principal/plataforma/requests/general_requests.py
+++ sigss_principal/plataforma/requests/general_requests.py
@@ -5,28 +5,22 @@ import json
 
 
 def show_general_mant(request):
-    if request.method == 'POST':
-        try:
-            cursor = connection.cursor()
-            cursor.callproc("MOSTRAR_MANTENIMIENTO_PREVENTIVO")
-            get_info = cursor.fetchall()
-            print("Yo habia ponido mi informacion aqui:",get_info)
-            general = get_info if get_info else []
-            try:
-                print(general)
-                return JsonResponse({"msg": general}, status=200)
-            except ValueError as e:
-                print(e)
-                print("Nada dentro del try")
-                return HttpResponse("<h1>Nada</h1>")
-        except (InternalError, IntegrityError, InterfaceError, ProgrammingError) as e:
-            print(e)
-            print("Nada dentro de except")
-            return JsonResponse({"msg": "Error en el sistema"}, status=200)
-        except ValueError as e:
-            print(e)
-            print("Nada dentro de except valueerror")
-            return HttpResponse("<h1>Nada</h1>")
+    try:
+        cursor = connection.cursor()
+        cursor.callproc("MOSTRAR_MANTENIMIENTO_PREVENTIVO")
+        get_info = cursor.fetchall()
+        print("Yo habia ponido mi informacion aqui:",get_info)
+        return JsonResponse({"msg": get_info}, status=200)
+    except (InternalError, IntegrityError, InterfaceError, ProgrammingError) as e:
+        print(e)
+        print("Nada dentro de except")
+        return JsonResponse({"msg": "Error en el sistema"}, status=200)
+    except ValueError as e:
+        print(e)
+        print("Nada dentro de except valueerror")
+        return HttpResponse("<h1>Nada</h1>")
+    finally:
+        cursor.close()
 
 
 def create_general(request):
