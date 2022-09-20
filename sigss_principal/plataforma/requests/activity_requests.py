7707739eb34e6a083c4e2e87d6ce355776c8e54d
Erick <erickfco1999@gmail.com>
2022-09-20 03:32:59 -0500

    Creando funciones para buscar y eliminar actividades

--- sigss_principal/plataforma/requests/activity_requests.py
+++ sigss_principal/plataforma/requests/activity_requests.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse, HttpResponse
 from django.db import connection
-from django.db import InternalError, IntegrityError, InterfaceError, ProgrammingError
+from django.db import InternalError, IntegrityError, InterfaceError, ProgrammingError, DataError, OperationalError
 import json
 
 
@@ -44,11 +44,40 @@ def modify_activity(request):
             print(e)
             return JsonResponse({"status": "error","msg": "Error en el sistema"}, status=200)
 
+def search_activity(request):
+    if request.method == 'POST':
+        response = json.loads(request.body.decode("utf-8"))
+        try:
+            cursor = connection.cursor()
+            cursor.callproc("BUSCAR_ACTIVIDAD_EN_MANTENIMIENTO", [response.get("id_act")])
+            encontrado = cursor.fetchall()
+            print(encontrado)
+            if encontrado:
+                return JsonResponse({"msg": encontrado}, status=200)
+            else:
+                return JsonResponse({"msg": "No encontrado"}, status=200)
+        except (InternalError, IntegrityError, InterfaceError, ProgrammingError, DataError, OperationalError) as e:
+            print(e)
+            return JsonResponse({"msg": "Error en el sistema"}, status=200)
+
 def delete_activity(request):
     if request.methods == 'POST':
         response = json.loads(request.body.decode("utf-8"))
         try:
             cursor = connection.cursor()
-            cursor.execute("DELETE FROM actividades where codigo = %s", [])
+            cursor.execute("DELETE FROM actividades where codigo = %s", [response.get("id_act")])
+            return JsonResponse({"status": "success", "msg": "Actividad eliminada"}, status=200)
         except (ProgrammingError, InternalError, InterfaceError, IntegrityError) as e:
             print(e)
+            return JsonResponse({"status": "error", "msg": "Error en el sistema"}, status=200)
+
+def delete_activity_with_mant(request):
+    if request.methods == 'POST':
+        response = json.loads(request.body.decode("utf-8"))
+        try:
+            cursor = connection.cursor()
+            cursor.execute("ELIMINAR_ACTIVIDAD_CON_MANTENIMIENTO", [response.get("id_act")])
+            return JsonResponse({"status": "success", "msg": "Actividad eliminada por completo"}, status=200)
+        except (ProgrammingError, InternalError, InterfaceError, IntegrityError) as e:
+            print(e)
+            return JsonResponse({"status": "error", "msg": "Error en el sistema"}, status=200)
