0e4a890766d80fdedc8b732fd2a1a39bd143f284
Erick <erickfco1999@gmail.com>
2022-09-20 14:01:54 -0500

    Cambiando nombre de variable obtenida desde fetch

--- sigss_principal/plataforma/requests/items_requests.py
+++ sigss_principal/plataforma/requests/items_requests.py
@@ -17,12 +17,12 @@ def show_items(request):
 def create_item(request):
     if request.method == 'POST':
         responses = json.loads(request.body.decode("utf-8"))
-        print(responses.get("cod_item"))
-        print(responses.get("name_item"))
+        print(responses.get("cod_"))
+        print(responses.get("name_"))
         print(responses.get("quantity"))
         try:
             cursor = connection.cursor()
-            cursor.execute("INSERT INTO maquinas_equipos (maq_eq_id, n_item, cantidad) values(%s, %s, %s)", (responses.get("cod_item"), responses.get("name_item"), responses.get("quantity")))
+            cursor.execute("INSERT INTO maquinas_equipos (maq_eq_id, n_item, cantidad) values(%s, %s, %s)", (responses.get("cod_"), responses.get("name_"), responses.get("quantity")))
             return JsonResponse({"status": "success", "msg": "Item agregado"}, status=200)
         except (ProgrammingError, InternalError, InterfaceError, IntegrityError, DataError, DatabaseError, OperationalError) as e:
             print(e)
