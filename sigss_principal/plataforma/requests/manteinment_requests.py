06be59304e253fca190f1773c9d1d1c34eb8a194
Erick <erickfco1999@gmail.com>
2022-09-20 03:35:39 -0500

    Creando funcion para mostrar mantenimiento

--- sigss_principal/plataforma/requests/manteinment_requests.py
+++ sigss_principal/plataforma/requests/manteinment_requests.py
@@ -3,6 +3,16 @@ from django.db import connection
 from django.db import InternalError as errors, IntegrityError as errors, InterfaceError as errors, ProgrammingError as errors
 import json
 
+def show_corrective_mant(request):
+    try:
+        cursor = connection.cursor()
+        cursor.callproc("MOSTRAR_MANTENIMIENTO_CORRECTIVO")
+        get_info = cursor.fetchall()
+        return JsonResponse({"msg": get_info}, status=200)
+    except (errors) as e:
+        print(e)
+        return JsonResponse({"msg": None}, status=200)
+
 def create_manteinment(request):
     if request.method == 'POST':
         try:
