9c8cefdc3ca545be9efb5e55add12e6a9f90a106
Erick <erickfco1999@gmail.com>
2022-09-22 15:54:52 -0500

    Estableciendo sintaxis para ejecutar crud

--- sigss_principal/plataforma/requests/manteinment_requests.py
+++ sigss_principal/plataforma/requests/manteinment_requests.py
@@ -15,27 +15,33 @@ def show_corrective_mant(request):
 
 def create_manteinment(request):
     if request.method == 'POST':
+        responses = json.loads(request.body.decode("utf-8"))
         try:
             cursor = connection.cursor()
-            cursor.execute("")
+            cursor.execute("INSERT INTO mantenimiento_corr (maq_eq_id_fk, novedad_id_fk, fecha_reporte, mod_fallo_fk, fecha_entrega, contador_dias, observaciones) values(%s, %s, %s, %s, %s, %s, %s)", [responses.get("item"), responses.get("novedad"), responses.get("report_date"), responses.get("fail"), responses.get("rec_date"), responses.get("days"), responses.get("notes")])
+            return JsonResponse({"status": "success", "msg": "Mantenimiento correctivo agregado"}, status=200)
         except (errors) as e:
             print(e)
             return JsonResponse({"status": "error", "msg": "Error en el sistema"}, status=200)
 
 def modify_manteinment(request):
     if request.method == 'POST':
+        responses = json.loads(request.body.decode("utf-8"))
         try:
             cursor = connection.cursor()
-            cursor.execute("")
+            cursor.execute("UPDATE mantenimiento_corr SET maq_eq_id_fk = %s, novedad_id_fk = %s, fecha_reporte = %s, mod_fallo_fk = %s, fecha_entrega = %s, contador_dias = %s, observaciones = %s where id_corr = %s", [responses.get("item"), responses.get("novedad"), responses.get("report_date"), responses.get("fail"), responses.get("rec_date"), responses.get("days"), responses.get("notes"), responses.get("corr_id")])
+            return JsonResponse({"status": "success", "msg": "Mantenimiento correctivo actualizado"}, status=200)
         except (errors) as e:
             print(e)
             return JsonResponse({"status": "error", "msg": "Error en el sistema"}, status=200)
 
 def delete_manteinment(request):
     if request.method == 'POST':
+        responses = json.loads(request.body.decode("utf-8"))
         try:
             cursor = connection.cursor()
-            cursor.execute("")
+            cursor.execute("DELETE FROM mantenimiento_corr WHERE id_corr = %s", [responses.get("corr_id")])
+            return JsonResponse({"status": "success", "msg": "Mantenimiento correctivo eliminado"}, status=200)
         except (errors) as e:
             print(e)
             return JsonResponse({"status": "error", "msg": "Error en el sistema"}, status=200)
