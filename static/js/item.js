57f73875c7c2bc0a075dc9deb8c0dd87113b724e
Erick <erickfco1999@gmail.com>
2022-09-23 13:21:31 -0500

    modificando validacion de informacion existente

--- static/js/item.js
+++ static/js/item.js
@@ -47,7 +47,7 @@ async function showProviders () {
 console.log("Existe:", prov_box.getElementsByClassName("id"));
 console.log("Existe texto:", prov_box.getElementsByClassName("id").innerText);
 
-if(prov_box.getElementsByClassName("id").innerText != "") {
+if(prov_box.getElementsByClassName("id").length == 0) {
     let btnAppearForm = document.querySelector(".link-add");
 
     btnAppearForm.addEventListener("click", () => {
@@ -82,7 +82,7 @@ function hideUpdateForm() {
     box_update_form.style.visibility = "hidden";
     box_update_form.style.opacity = 0;
     form.reset();
-    if(prov_box.getElementsByClassName("id").innerText != "") {
+    if(prov_box.getElementsByClassName("id").length == 0) {
         if(btnAppearForm.style.display == 'none') {
             console.log("Display caja select:", btnAppearForm.style.display);
             btnAppearForm.style.display == 'block';
