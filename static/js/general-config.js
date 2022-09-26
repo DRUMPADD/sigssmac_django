6aa3c876feabc4bb4c624e72324d96b02171f8eb
Erick Olvera <erickfco1999@gmail.com>
2022-09-26 13:28:07 -0500

    Cambiando configuracion de textarea a otro archivo

--- static/js/general-config.js
+++ static/js/general-config.js
@@ -3,26 +3,3 @@ document.body.querySelectorAll("[hidden]").forEach((el, i) => {
         el.style.display = 'none';
     }
 })
-
-let textarea = document.querySelector(".textarea");
-let current_value = document.getElementById("actual");
-let total_value = document.getElementById("completo");
-
-textarea.addEventListener("keyup", (e) => {
-    let current = e.target.value.length;
-    
-    if(current <= 100) {
-        current_value.style.color = "#000";
-        total_value.style.color = "#000";
-    } else if(current > 100 && current < 200) {
-        current_value.style.color = "#a9ac03";
-        total_value.style.color = "#a9ac03";
-    } else if (current > 270) {
-        current_value.style.color = "#da0101";
-        total_value.style.color = "#da0101";
-    }
-
-    if (current < 301) {
-        current_value.innerText = current;
-    }
-})
