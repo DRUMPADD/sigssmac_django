800dd0389640accf3889bb264bf1a526d7ac7f32
Erick Olvera <erickfco1999@gmail.com>
2022-09-27 11:56:38 -0500

    Validacion para tomar datos de acuerdo a los formularios que aparezcan

--- static/js/item.js
+++ static/js/item.js
@@ -47,6 +47,60 @@ if(prov_box.getElementsByClassName("id").length == 0) {
     window.addEventListener("DOMContentLoaded", () => {
         showProviders();
     })
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let ar_answers = new Array();
+        if(form.elements[2].value != "--Seleccionar--" && form.elements[2].value == "NUEVO") {
+            for(let i = 0; i < form.elements.length - 1; i++) {
+                if(form.elements[i].type != "button" && form.elements[i].type != "submit") {
+                    ar_answers.push(form.elements[i].value)
+                }
+            }
+        } else {
+            for(let i = 0; i < form.elements.length - 6; i++) {
+                if(form.elements[i].type != "button" && form.elements[i].type != "submit") {
+                    ar_answers.push(form.elements[i].value)
+                }
+            }
+        }
+        if(ar_answers.length > 3) {
+            ar_answers.splice(2, 1)
+            registerProvider({
+                item_id: ar_answers[1],
+                prov_id: ar_answers[2],
+                p_nombre: ar_answers[3],
+                numero_t: ar_answers[4],
+                _email: ar_answers[5],
+                country: ar_answers[6],
+            })
+        } else {
+            registerProvider({
+                item_id: ar_answers[1],
+                prov_id: ar_answers[2]
+            })
+        }
+    })
+} else {
+    let btn_mod = document.querySelector(".select-mod");
+    let btn_del = document.querySelector(".select-del");
+    let i_del = document.querySelector(".del");
+    let inputs_ = document.querySelectorAll(".input_prov");
+
+    btn_mod.addEventListener("click", () => {
+        inputs_.forEach(element => {
+            element.removeAttribute("disabled");
+        })
+        i_del.classList.remove("fa-trash-can");
+        i_del.classList.add("fa-x");
+    })
+
+    btn_del.addEventListener("click", () => {
+        inputs_.forEach(element => {
+            element.setAttribute("disabled", "");
+        })
+        i_del.classList.remove("fa-x");
+        i_del.classList.add("fa-trash-can");
+    })
 }
 
 function getCookie(name) {
@@ -98,59 +152,3 @@ function registerProvider(values) {
         })
     })
 }
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let ar_answers = new Array();
-    if(form.elements[2].value != "--Seleccionar--" && form.elements[2].value == "NUEVO") {
-        for(let i = 0; i < form.elements.length - 1; i++) {
-            if(form.elements[i].type != "button" && form.elements[i].type != "submit") {
-                ar_answers.push(form.elements[i].value)
-            }
-        }
-    } else {
-        for(let i = 0; i < form.elements.length - 6; i++) {
-            if(form.elements[i].type != "button" && form.elements[i].type != "submit") {
-                ar_answers.push(form.elements[i].value)
-            }
-        }
-    }
-    if(ar_answers.length > 3) {
-        ar_answers.splice(2, 1)
-        registerProvider({
-            item_id: ar_answers[1],
-            prov_id: ar_answers[2],
-            p_nombre: ar_answers[3],
-            numero_t: ar_answers[4],
-            _email: ar_answers[5],
-            country: ar_answers[6],
-        })
-    } else {
-        registerProvider({
-            item_id: ar_answers[1],
-            prov_id: ar_answers[2]
-        })
-    }
-})
-
-
-let btn_mod = document.querySelector(".select-mod");
-let btn_del = document.querySelector(".select-del");
-let i_del = document.querySelector(".del");
-let inputs_ = document.querySelectorAll(".input_prov");
-
-btn_mod.addEventListener("click", () => {
-    inputs_.forEach(element => {
-        element.removeAttribute("disabled");
-    })
-    i_del.classList.remove("fa-trash-can");
-    i_del.classList.add("fa-x");
-})
-
-btn_del.addEventListener("click", () => {
-    inputs_.forEach(element => {
-        element.setAttribute("disabled", "");
-    })
-    i_del.classList.remove("fa-x");
-    i_del.classList.add("fa-trash-can");
-})
