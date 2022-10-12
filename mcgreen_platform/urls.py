c95a42d940238b9b60ce43c612a7a9a1fb37d84c
Erick Olvera <erickfco1999@gmail.com>
2022-10-12 10:21:13 -0500

    Corrigiendo error en nombre de URL

--- mcgreen_platform/urls.py
+++ mcgreen_platform/urls.py
@@ -3,7 +3,7 @@ from mcgreen_platform.views import *
 
 urlpatterns = [
     path("", index, name="generalMantMCGREEN"),
-    path("actividades", activities, name="activitiesMCGREEN  "),
+    path("actividades", activities, name="activitiesMCGREEN"),
     path("equipos", items, name="itemsMCGREEN"),
     path("mantenimiento-correctivo", correc_manteinment, name="corMantMCGREEN"),
     path("otros-registros", other_views, name="otherRegistersMCGREEN"),
