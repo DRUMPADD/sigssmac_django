c0290ddcc0ff72fc28ff58e77c1355652a0fdd01
Erick Olvera <erickfco1999@gmail.com>
2022-10-12 15:29:43 -0500

    Agregando funcion para mostrar item y modificando funcion para otros movimientos

--- mcgreen_platform/views.py
+++ mcgreen_platform/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.template import TemplateDoesNotExist, TemplateSyntaxError
+from django.db import connections
+from django.db import OperationalError, DatabaseError, ProgrammingError
 # Create your views here.
 def index(request):
     context = {
@@ -19,14 +21,71 @@ def items(request):
     }
     return render(request, "mcgreen/items.html", context)
 
+def item_view(request, id_item):
+    context = {
+        "item": id_item
+    }
+    print("ID:",id_item)
+    try:
+        cursor = connections["mcgreen_db"].cursor()
+        cursor.callproc("CARACTERISTICAS_ITEM", [id_item])
+        get_info = cursor.fetchall()
+        ar = []
+        for car in get_info:
+            if car[1] == id_item:
+                ar.append(car)
+        context["caracteristicas"] = ar
+        context["title"] = 'Item ' + id_item
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+    try:
+        cursor = connections["mcgreen_db"].cursor()
+        cursor.execute("SELECT * FROM estado_plat")
+        get_info = cursor.fetchall()
+        context["states"] = get_info
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+    try:
+        cursor2 = connections["mcgreen_db"].cursor()
+        cursor2.callproc("MOSTRAR_PROVEEDOR_EN_ITEM", [id_item])
+        get_pro = cursor2.fetchall()
+        context["existe_prov"] = True if get_pro else False
+        context["provider"] = get_pro
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor2.close()
+    try:
+        cursor = connections["mcgreen_db"].cursor()
+        cursor.execute("SELECT * FROM proveedores")
+        get_pro = cursor.fetchall()
+        context["providers"] = get_pro
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+    try:
+        return render(request, "mcgreen/item.html", context)
+    except (TemplateDoesNotExist, TemplateSyntaxError) as er:
+        print("Error en template:",er)
+        return render(request, "plataforma/template_error.html")
+
 def correc_manteinment(request):
     context = {
         "title": "Mantenimiento correctivo"
     }
     return render(request, "mcgreen/manteinment.html", context)
 
-def other_views(request):
-    context = {
-        "title": "Otros registros"
-    }
-    return render(request, "tarco/other.html", context)
+def other_view(request):
+    try:
+        context = {
+            "title": "Otros movimientos"
+        }
+        return render(request, "mcgreen/other.html", context)
+    except (TemplateDoesNotExist, TemplateSyntaxError) as er:
+        print("Error en template:",er)
+        return render(request, "plataforma/template_error.html")
--- tarco_platform/views.py
+++ tarco_platform/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.template import TemplateDoesNotExist, TemplateSyntaxError
+from django.db import connections
+from django.db import OperationalError, DatabaseError, ProgrammingError
 
 # Create your views here.
 def index(request):
@@ -19,6 +22,59 @@ def items(request):
     }
     return render(request, "tarco/items.html", context)
 
+def item_view(request, id_item):
+    context = {
+        "item": id_item
+    }
+    print("ID:",id_item)
+    try:
+        cursor = connections["tarco_db"].cursor()
+        cursor.callproc("CARACTERISTICAS_ITEM", [id_item])
+        get_info = cursor.fetchall()
+        ar = []
+        for car in get_info:
+            if car[1] == id_item:
+                ar.append(car)
+        context["caracteristicas"] = ar
+        context["title"] = 'Item ' + id_item
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+    try:
+        cursor = connections["tarco_db"].cursor()
+        cursor.execute("SELECT * FROM estado_plat")
+        get_info = cursor.fetchall()
+        context["states"] = get_info
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+    try:
+        cursor2 = connections["tarco_db"].cursor()
+        cursor2.callproc("MOSTRAR_PROVEEDOR_EN_ITEM", [id_item])
+        get_pro = cursor2.fetchall()
+        context["existe_prov"] = True if get_pro else False
+        context["provider"] = get_pro
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor2.close()
+    try:
+        cursor = connections["tarco_db"].cursor()
+        cursor.execute("SELECT * FROM proveedores")
+        get_pro = cursor.fetchall()
+        context["providers"] = get_pro
+    except (OperationalError, DatabaseError, ProgrammingError) as e:
+        print(e)
+    finally:
+        cursor.close()
+    try:
+        return render(request, "tarco/item.html", context)
+    except (TemplateDoesNotExist, TemplateSyntaxError) as er:
+        print("Error en template:",er)
+        return render(request, "plataforma/template_error.html")
+
 def correc_manteinment(request):
     context = {
         "title": "Mantenimiento correctivo"
@@ -26,7 +82,11 @@ def correc_manteinment(request):
     return render(request, "tarco/manteinment.html", context)
 
 def other_views(request):
-    context = {
-        "title": "Otros registros"
-    }
-    return render(request, "tarco/other.html", context)
+    try:
+        context = {
+            "title": "Otros movimientos"
+        }
+        return render(request, "tarco/other.html", context)
+    except (TemplateDoesNotExist, TemplateSyntaxError) as er:
+        print("Error en template:",er)
+        return render(request, "plataforma/template_error.html")
