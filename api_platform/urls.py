4008336a6944fa2ef211f8441d658e8b38934902
Erick Olvera <erickfco1999@gmail.com>
2022-10-17 13:31:24 -0500

    Agregando urls de vistas y peticiones

--- api_platform/urls.py
+++ api_platform/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from api_platform.views import *
+from api_platform.accidents import *
 urlpatterns = [
     path("", index, name="generalMantAPI"),
     path("/actividades", activities, name="activitiesAPI"),
     path("/equipos", items, name="itemsAPI"),
     path("/mantenimiento-correctivo", correc_manteinment, name="corMantAPI"),
     path("/otros-registros", other_views, name="otherRegistersAPI"),
+
+    # ?? Accidentabilidad
+    path("/accidentabilidad", accidentability_view, name="accidentabilityAPI"),
+    path("datos_generales_obtenidos", data_every_month, name="datos_generales_obtenidos"),
+    path("registrar_accidente", register_accident, name="registrar_info"),
 ]
--- tarco_platform/urls.py
+++ tarco_platform/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from tarco_platform.views import *
+from tarco_platform.accidents import *
 urlpatterns = [
     path("", index, name="generalMantTARCO"),
     path("/actividades", activities, name="activitiesTARCO"),
     path("/equipos", items, name="itemsTARCO"),
     path("/mantenimiento-correctivo", correc_manteinment, name="corMantTARCO"),
     path("/otros-registros", other_views, name="otherRegistersTARCO"),
+
+
+    # ?? Accidentabilidad
+    path("/accidentabilidad", accidentability_view, name="accidentabilityTARCO"),
+    path("datos_generales_obtenidos", data_every_month, name="datos_generales_obtenidos"),
+    path("registrar_acci", register_accident, name="registrar_info"),
 ]
