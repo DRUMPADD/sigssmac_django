20910f5c33613dbd9d64079cec7f3527fb38db59
Erick Olvera <erickfco1999@gmail.com>
2022-10-17 15:28:26 -0500

    Correccion de conexiones a bases de datos para evitar equivocaciones

--- tarco_platform/accidents.py
+++ tarco_platform/accidents.py
@@ -5,10 +5,9 @@ import json
 def register_accident(request):
     if request.method == 'POST':
         res = json.loads(request.body.decode('utf-8'))
-        mensaje = ""
         print(res)
         try:
-            cursor = connections["api_energy_db"].cursor()
+            cursor = connections["tarco_db"].cursor()
             cursor.callproc("ACCIDENTABILIDAD", [res["c_personal"][0], res["h_trabajo"][0], res["jornada"][0], res["c_personal"][1], res["h_trabajo"][1], res["jornada"][1], res["mes"], res["anio"], res["acci_c_baja"][0], res["acci_c_baja"][1], res["dias_inc"][0], res["dias_inc"][1]])
             return JsonResponse({"status": "success", "res": "Registro realizado"}, status=200)
         except (DatabaseError, IntegrityError, NotSupportedError, InterfaceError, OperationalError, ProgrammingError) as e:
