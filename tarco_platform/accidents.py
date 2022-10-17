214d39b4737bc8e8a84f99221e1f61545347d442
Erick Olvera <erickfco1999@gmail.com>
2022-10-17 13:17:50 -0500

    Peticion post para registrar accidentes

---
+++ tarco_platform/accidents.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.db import connections
+from django.db import DatabaseError, IntegrityError, NotSupportedError, InterfaceError, OperationalError, ProgrammingError
+import json
+def register_accident(request):
+    if request.method == 'POST':
+        res = json.loads(request.body.decode('utf-8'))
+        mensaje = ""
+        print(res)
+        try:
+            cursor = connections["api_energy_db"].cursor()
+            cursor.callproc("ACCIDENTABILIDAD", [res["c_personal"][0], res["h_trabajo"][0], res["jornada"][0], res["c_personal"][1], res["h_trabajo"][1], res["jornada"][1], res["mes"], res["anio"], res["acci_c_baja"][0], res["acci_c_baja"][1], res["dias_inc"][0], res["dias_inc"][1]])
+            mensaje = cursor.fetchall()[0]
+            print(mensaje)
+            return JsonResponse({"status": "success", "res": mensaje}, status=200)
+        except (DatabaseError, IntegrityError, NotSupportedError, InterfaceError, OperationalError, ProgrammingError) as e:
+            print(e)
+            return JsonResponse({"status": "error", "res": "Ocurrió un error en el sistema"}, status=200)
