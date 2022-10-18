507a0d5dcb20f7c62bad6df79275ba0883cb4e1d
Erick Olvera <erickfco1999@gmail.com>
2022-10-18 11:54:48 -0500

    Modificando parametros a pasar a procedimiento almacenado

--- tarco_platform/accidents.py
+++ tarco_platform/accidents.py
@@ -8,7 +8,7 @@ def register_accident(request):
         print(res)
         try:
             cursor = connections["tarco_db"].cursor()
-            cursor.callproc("ACCIDENTABILIDAD", [int(res["c_personal"][0]), int(res["h_trabajo"][0]), int(res["jornada"][0]), int(res["c_personal"][1]), int(res["h_trabajo"][1]), int(res["jornada"][1]), res["mes"], res["anio"], int(res["acci_c_baja"][0]), int(res["acci_c_baja"][1]), int(res["dias_inc"][0]), int(res["dias_inc"][1])])
+            cursor.callproc("ACCIDENTABILIDAD", [res["c_personal"][0], res["h_trabajo"][0], res["jornada"][0], res["c_personal"][1], res["h_trabajo"][1], res["jornada"][1], res["mes"], res["anio"], res["acci_c_baja"][0], res["acci_c_baja"][1], res["dias_inc"][0], res["dias_inc"][1]])
             return JsonResponse({"status": "success", "res": "Registro realizado"}, status=200)
         except (DatabaseError, IntegrityError, NotSupportedError, InterfaceError, OperationalError, ProgrammingError) as e:
             print(e)
