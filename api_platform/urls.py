65385d9e7e790529d815b53edd4ac8c95fa4c14a
Erick Olvera <erickfco1999@gmail.com>
2022-10-18 10:28:27 -0500

    Corrigiendo error de urls

--- api_platform/urls.py
+++ api_platform/urls.py
@@ -3,13 +3,13 @@ from api_platform.views import *
 from api_platform.accidents import *
 urlpatterns = [
     path("", index, name="generalMantAPI"),
-    path("/actividades", activities, name="activitiesAPI"),
-    path("/equipos", items, name="itemsAPI"),
-    path("/mantenimiento-correctivo", correc_manteinment, name="corMantAPI"),
-    path("/otros-registros", other_views, name="otherRegistersAPI"),
+    path("actividades", activities, name="activitiesAPI"),
+    path("equipos", items, name="itemsAPI"),
+    path("mantenimiento-correctivo", correc_manteinment, name="corMantAPI"),
+    path("otros-registros", other_views, name="otherRegistersAPI"),
 
     # ?? Accidentabilidad
-    path("/accidentabilidad", accidentability_view, name="accidentabilityAPI"),
-    path("/datos_generales_obtenidos", data_every_month, name="datos_generales_obtenidos"),
-    path("/registrar_accidente", register_accident, name="registrar_info"),
+    path("accidentabilidad", accidentability_view, name="accidentabilityAPI"),
+    path("datos_generales_obtenidos", data_every_month, name="datos_generales_obtenidos"),
+    path("registrar_accidente", register_accident, name="registrar_info"),
 ]
--- mcgreen_platform/urls.py
+++ mcgreen_platform/urls.py
@@ -3,13 +3,13 @@ from mcgreen_platform.views import *
 from mcgreen_platform.accidents import *
 urlpatterns = [
     path("", index, name="generalMantMCGREEN"),
-    path("/actividades", activities, name="activitiesMCGREEN"),
-    path("/equipos", items, name="itemsMCGREEN"),
-    path("/mantenimiento-correctivo", correc_manteinment, name="corMantMCGREEN"),
-    path("/otros-registros", other_view, name="otherRegistersMCGREEN"),
+    path("actividades", activities, name="activitiesMCGREEN"),
+    path("equipos", items, name="itemsMCGREEN"),
+    path("mantenimiento-correctivo", correc_manteinment, name="corMantMCGREEN"),
+    path("otros-registros", other_view, name="otherRegistersMCGREEN"),
 
     # ?? Accidentabilidad
-    path("/accidentabilidad", accidentability_view, name="accidentabilityMCGREEN"),
-    path("/datos_generales_obtenidos", data_every_month, name="datos_generales_obtenidos"),
-    path("/registrar_accidente", register_accident, name="registrar_info"),
+    path("accidentabilidad", accidentability_view, name="accidentabilityMCGREEN"),
+    path("datos_generales_obtenidos", data_every_month, name="datos_generales_obtenidos"),
+    path("registrar_accidente", register_accident, name="registrar_info"),
 ]
